3330f6712d7bf808896926a391076ed591b245c5
valentinaboya152 <valentinaboya15@gmail.com>
2025-10-30 22:05:54 -0500

    EDIT: filter by room name in RoomView

--- pms/urls.py
+++ pms/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from .views import RoomsView
 urlpatterns = [
     path("",views.HomeView.as_view(),name = "home"),
     path("search/room/",views.RoomSearchView.as_view(),name = "search"),
@@ -8,7 +8,7 @@ urlpatterns = [
     path("booking/<str:pk>/",views.BookingView.as_view(),name = "booking"),
     path("booking/<str:pk>/edit",views.EditBookingView.as_view(),name = "edit_booking"),
     path("booking/<str:pk>/delete",views.DeleteBookingView.as_view(),name = "delete_booking"),
-    path("rooms/",views.RoomsView.as_view(),name = "rooms"),
+    path('rooms/', RoomsView.as_view(), name='rooms'),
     path("room/<str:pk>/",views.RoomDetailsView.as_view(),name = "room_details"),
     path("dashboard/",views.DashboardView.as_view(),name = "dashboard")
 ]
